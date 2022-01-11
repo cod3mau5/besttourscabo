69dec66f9a349ea13478b21baf0f8a4ccd7c31aa
cod3mau5 <code.bit.mau@gmail.com>
2022-01-10 18:33:41 -0700

    logo added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <div id="app">
+        <header>
+            <div class="container">
+                <div class="row">
+                    <img src="{{ asset('assets/img/best_tours_cabo_horizontal.png') }}" alt="">
+                </div>
+            </div>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</body>
+</html>
