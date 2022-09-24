a9beadb0b13f19126c42935268b0420fcb406ffe
cod3mau5 <code.bit.mau@gmail.com>
2022-09-24 10:46:52 -0600

    more css added to the tours page

--- resources/views/pages/traditional_arch_tour.blade.php
+++ resources/views/pages/traditional_arch_tour.blade.php
@@ -1,5 +1,6 @@
 @extends('pages.layouts.master')
 @section('header_scripts')
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@900&display=swap" rel="stylesheet">
     <!-- Link Swiper's CSS -->
     <link
       rel="stylesheet"
@@ -9,9 +10,20 @@
         :root{
             --main_blue:#023047;
           --main_orange:#ffb703;
+          --secondary_orange:#ff7e00;
         }
         main{
             margin-top: 115px;
+        }
+        .tour-title {
+            text-align: center!important;
+            font-family: 'Work Sans', sans-serif;
+            background: radial-gradient(circle, var(--main_orange) 10%, var(--secondary_orange) 100%);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            margin: 0;
+            font-size: 42px;
+            background-color: var(--main_blue);
         }
         .grid-gallery > div{
             width: 100%;
@@ -65,9 +77,8 @@
                 grid-column-end: 4
             }
             .tour-title {
-                text-align: center;
-                padding-top: 1rem!important;
-                margin-bottom:2rem!important;
+                padding-top: 1rem;
+                margin-bottom:2rem;
             }
 
         }
@@ -137,6 +148,56 @@
         background-color: #555;
         }
 
+        .card-price {
+            display: inline-block;
+            width: auto;
+            height: 38px;
+            background-color: #6ab070;
+            -webkit-border-radius: 3px 4px 4px 3px;
+            -moz-border-radius: 3px 4px 4px 3px;
+            border-radius: 3px 4px 4px 3px;
+            border-left: 1px solid #6ab070;
+
+            /* This makes room for the triangle */
+            margin-left: 19px;
+            position: relative;
+            color: white;
+            font-weight: 300;
+            font-size: 22px;
+            line-height: 38px;
+            padding: 0 10px 0 10px;
+        }
+
+        /* Makes the triangle */
+        .card-price:before {
+            content: "";
+            position: absolute;
+            display: block;
+            left: -19px;
+            width: 0;
+            height: 0;
+            border-top: 19px solid transparent;
+            border-bottom: 19px solid transparent;
+            border-right: 19px solid #6ab070;
+        }
+
+        /* Makes the circle */
+        .card-price:after {
+            content: "";
+            background-color: white;
+            border-radius: 50%;
+            width: 4px;
+            height: 4px;
+            display: block;
+            position: absolute;
+            left: -9px;
+            top: 17px;
+        }
+
+        .description h1{
+            text-align: center;
+        }
+
         @media only screen and (min-width:1200px) {}
         @media only screen and (min-width:1100px) {}
         @media only screen and (max-width:920px) {}
@@ -173,6 +234,7 @@
     <main id="traditional_arch_tour">
         <div class="ui container">
 
+            <h2 class="tour-title">TRADITIONAL ARCH TOUR</h2>
 
             <div class="swiper mySwiper">
                 <div class="grid-gallery swiper-wrapper">
@@ -197,10 +259,15 @@
                 <li class="">Selection</li>
                 <li class="">Date</li>
             </ol>
-
-            <h2 class="tour-title">TRADITIONAL ARCH TOUR</h2>
-
-
+            <div class="ui two column grid">
+                <div class="row">
+                    <div class="column">Amenities</div>
+                    <div class="column">
+                        Price
+                        <p class="card-price">1250 р.</p>
+                    </div>
+                </div>
+            </div>
             <div class="description">
                 {{-- <h1>Descripción</h1>
                 <p class="tour-description">
@@ -208,18 +275,43 @@
                     Durante este paseo visitamos la famosa formación rocosa de Cabo San Lucas "El Arco", junto con otros puntos atractivos de esa zona como lo son: la colonia de leones marinos, La Roca del Pelícano, El Dedo de Neptuno, La Playa del Amor y del Divorcio y sus alrededores. Tendrá la oportunidad de observar algunas de las especies marinas que habitan este pedacito de mar.<br><br>
                     Una vez que el paseo finaliza, usted tendrá la opción si lo desea, de quedarse el tiempo que guste en la Playa del Amor, donde podrá caminar, nadar o asolearse. Usted podrá regresar en cualquiera de las lanchas que vienen de regreso a la marina cada hora, la última que es a las 5 o 6pm dependiendo la temporada.
                 </p> --}}
-                <h1>Description</h1>
+                <h1>About this tour</h1>
                 <p class="tour-description">
-                    One of the Easiest and Most Pleasant Tours in Cabo!...<br>
-                    During this tour we visit the famous rock formation of Cabo San Lucas "El Arco", along with other attractive points in that area such as: the sea lion colony, Pelican Rock, Neptune's Finger, Love Beach and of Divorce and its surroundings. You will have the opportunity to observe some of the marine species that inhabit this little piece of the sea.<br><br>
-                    Once the tour ends, you will have the option, if you wish, to stay as long as you like at Playa del Amor, where you can walk, swim or sunbathe. You can return in any of the boats that come back to the marina every hour, the last one being at 5 or 6pm depending on the season.
+
                 </p>
+                <div class="ui styled fluid accordion">
+                    <div class="title active">
+                      <i class="dropdown icon"></i>
+                      Tour description
+                    </div>
+                    <div class="content active">
+                      <p class="transition visible">
+                        One of the Easiest and Most Pleasant Tours in Cabo!...<br>
+                        During this tour we visit the famous rock formation of Cabo San Lucas "El Arco", along with other attractive points in that area such as: the sea lion colony, Pelican Rock, Neptune's Finger, Love Beach and of Divorce and its surroundings. You will have the opportunity to observe some of the marine species that inhabit this little piece of the sea.<br><br>
+                        Once the tour ends, you will have the option, if you wish, to stay as long as you like at Playa del Amor, where you can walk, swim or sunbathe. You can return in any of the boats that come back to the marina every hour, the last one being at 5 or 6pm depending on the season.</p>
+                    </div>
+                    <div class="title">
+                      <i class="dropdown icon"></i>
+                      What kinds of dogs are there?
+                    </div>
+                    <div class="content">
+                      <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+                    </div>
+                    <div class="title">
+                      <i class="dropdown icon"></i>
+                      How do you acquire a dog?
+                    </div>
+                    <div class="content">
+                      <p class="transition hidden">Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
+                      <p class="transition hidden">A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+                    </div>
+                  </div>
             </div>
 
             <div class="button-container">
                 <button class="ui right labeled icon button">
                     <i class="right arrow icon"></i>
-                    Next
+                    Next Step
                 </button>
             </div>
 
@@ -298,6 +390,7 @@
             if(width < 921){
                 $(window).trigger('resize');
             }
+            $('.ui.accordion').accordion();
         });
 
 
